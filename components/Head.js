da2900a512d6bda34f4978f7023fe396f9003e85
Federico Vitale <fedevitale99@gmail.com>
2018-10-15 09:41:49 +0200

    Update Head.js

--- components/Head.js
+++ components/Head.js
@@ -6,7 +6,6 @@ export default class CustomHead extends Component {
         return (
             <div>
                 <Head>
-                    <meta charset="UTF-8" />
                     <meta name="viewport" content="width=device-width, initial-scale=0.6" />
                     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
                     <meta name="theme-color" content="#F8F6F7"/>
