597f35e09226dfdbfea90eb36e9aade427068f0f
Federico Vitale <fedevitale99@gmail.com>
2019-04-01 22:01:41 +0200

    deployment

---
+++ .travis/deploy.sh
@@ -0,0 +1,16 @@
+#! /bin/bash;
+
+eval "$(ssh-agent -s)"
+chmod 600 .travis/id_rsa
+ssh-add .travis/id_rsa
+
+git config --global push.default matching
+git remote add deploy ssh://git@rawnly.com:22$DEPLOY_DIR
+git push origin master
+
+ssh root@$IP <<EOF
+	cd $DEPLOY_DIR;
+	yarn build;
+	yarn start;
+	pm2 reload splash-cli.app;
+EOF
