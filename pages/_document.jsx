2b1b869137d533c185bbf7c3244b9ec5c60ebec3
Federico Vitale <fedevitale99@gmail.com>
2020-02-11 14:47:32 +0100

    ADS on production only

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,7 +13,9 @@ class MyDocument extends Document {
 				<Head>
 					<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
 
-					<script src='https://app.codefund.io/properties/690/funder.js' async></script>
+					{process.env.NODE_ENV === 'production' && (
+						<script src='https://app.codefund.io/properties/690/funder.js' async></script>
+					)}
 				</Head>
 				<body>
 					<Main />
